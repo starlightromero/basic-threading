936818d138becc87217e67a1974bb1d17efba677
Starlight Romero <starlightromero@gmail.com>
2020-12-18 18:44:38 -0800

    [update] threading to concurrent.futures module

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import time
-import threading
+import concurrent.futures
 
 start = time.perf_counter()
 
@@ -7,18 +7,13 @@ start = time.perf_counter()
 def do_something(seconds):
     print(f"Sleeping {seconds} second(s)...")
     time.sleep(seconds)
-    print("Done Sleeping")
+    return "Done Sleeping"
 
 
-threads = []
-
-for _ in range(10):
-    t = threading.Thread(target=do_something, args=[1.5])
-    t.start()
-    threads.append(t)
-
-for thread in threads:
-    thread.join()
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    results = [executor.submit(do_something, 1) for _ in range(10)]
+    for f in concurrent.futures.as_completed(results):
+        print(f.result())
 
 finish = time.perf_counter()
 
